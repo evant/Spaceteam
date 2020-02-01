8a3d57299b00a4715f8f0f7b97d9873894002aff
Jeff Ward <jeff@fuzzybinary.com>
2020-02-01 14:26:27 -0500

    Things now get set on fire

---
+++ Assets/Scripts/MainGameLogic.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGameLogic : MonoBehaviour
+{
+    public float fireSpawnTime = 8.0f;
+    public GameObject firePrefab;
+
+    private float _timeToNextFire;
+
+    void Start()
+    {
+        _timeToNextFire = fireSpawnTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (firePrefab != null)
+        {
+            _timeToNextFire -= Time.deltaTime;
+            if (_timeToNextFire < 0)
+            {
+                var spawns = FindObjectsOfType<FireSpawnPoint>();
+                for (int i = 0; i < 5; ++i)
+                {
+                    var index = Random.Range(0, spawns.Length);
+                    var spawnPoint = spawns[index];
+                    if (spawnPoint.transform.childCount == 0)
+                    {
+                        Object.Instantiate(firePrefab, spawnPoint.transform);
+                        break;
+                    }
+                }
+                _timeToNextFire = fireSpawnTime;
+            }
+        }
+    }
+}
