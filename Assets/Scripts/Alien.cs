f8de1ad1250706ddefa64c8ff56c08100e881f98
Alexia Bernardo <me@alexiabernardo.com>
2020-02-01 18:27:31 -0500

    add alien guy

---
+++ Assets/Scripts/Alien.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alien : MonoBehaviour
+{
+    public Transform player;
+    public float moveSpeed = 1f;
+    private Rigidbody2D rb2D;
+    private Vector2 movement;
+
+    void Start()
+    {
+         rb2D = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        Vector3 direction = player.position - transform.position;
+        direction.Normalize();
+        movement = direction;
+    }
+
+    private void FixedUpdate()
+    {
+        moveAlien(movement);
+    }
+
+    void moveAlien(Vector2 direction)
+    {
+        rb2D.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+}
