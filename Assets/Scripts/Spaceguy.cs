0f4977c999a5993f9e47f147a88ec3e9f83aedd0
Evan Tatarka <evan.tatarka@willowtreeapps.com>
2020-02-01 12:36:08 -0500

    Moving spaceguy

---
+++ Assets/Scripts/Spaceguy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Spaceguy : MonoBehaviour
+{
+    public float speed = 6.0f;
+    private Vector3 moveDirection = Vector3.zero;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveDirection *= speed;
+        gameObject.transform.position += moveDirection * Time.deltaTime;
+    }
+}
