1c2a1996e5d9feaafaeb91c67cd83517735664d9
Alexia Bernardo <me@alexiabernardo.com>
2020-02-01 20:52:43 -0500

    added menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+     public void playGame() {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void loadMenu(){
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void loadInstructions(){
+        SceneManager.LoadScene("Instructions");
+    }
+ 
+    public void exitGame() {
+        Application.Quit();
+    }
+}
